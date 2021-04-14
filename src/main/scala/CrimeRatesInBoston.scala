0ba9183d744d0ca141692ef63c9241aabb493628
salimov-bilol <salimovb1999@gmail.com>
2021-04-14 18:03:57 +0500

    Crime Rates in Boston Completed

--- src/main/scala/CrimeRatesInBoston.scala
+++ src/main/scala/CrimeRatesInBoston.scala
@@ -1,4 +1,3 @@
-import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
@@ -6,35 +5,73 @@ object CrimeRatesInBoston {
 
   def main(args: Array[String]): Unit = {
 
-    Logger.getLogger("org").setLevel(Level.ERROR)
-
     val conf = new SparkConf()
     conf.setMaster("local[*]")
     conf.setAppName("Crimes in Boston")
-    conf.set("spark.sql.warehouse.dir", "D:\\IdeaProjects\\MySparkJobs\\spark-warehouse")
-
-    val spark = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
+    val spark = SparkSession.builder().config(conf).getOrCreate()
 
     val df1 = spark
       .read
       .option("inferSchema", "true")
       .option("header", "true")
-      .csv("D:\\IdeaProjects\\MySparkJobs\\data\\crime.csv")
-
+      .csv("D:/IdeaProjects/MySparkJobs/data/crime.csv")
+    df1.printSchema()
     val df2 = spark
       .read
       .option("inferSchema", "true")
       .option("header", "true")
-      .csv("D:\\IdeaProjects\\MySparkJobs\\data\\offense_codes.csv")
-
+      .csv("D:/IdeaProjects/MySparkJobs/data/offense_codes.csv")
+    df2.printSchema()
     import spark.implicits._
 
-    val crimes = df1.select($"OFFENSE_CODE", $"DISTRICT", $"STREET")
-    val offenseCodes = df2.select($"CODE".as("OFFENSE_CODE"), $"NAME").dropDuplicates("OFFENSE_CODE")
+    val crimes = df1
+      .select($"OFFENSE_CODE", $"DISTRICT", $"STREET")
+      .na
+      .drop()
 
-    crimes.show(false)
-    offenseCodes.show(truncate = false)
+    val offenseCodes = df2
+      .select($"CODE".as("OFFENSE_CODE"), $"NAME")
+      .dropDuplicates("OFFENSE_CODE")
 
+    val crimesCountByDistrictArray = crimes
+      .groupBy($"DISTRICT")
+      .count()
+      .collect()
+
+    var crimesCountByDistrictMap = Map[String, Long]()
+
+    for (row <- crimesCountByDistrictArray) {
+      crimesCountByDistrictMap += (row.getString(0) -> row.getLong(1))
+    }
+
+    val broadcastVariable = spark.sparkContext.broadcast(crimesCountByDistrictMap)
+
+    import org.apache.spark.sql.functions._
+
+    val myUDF = udf
+    {
+      district: String =>
+      {
+        val total = broadcastVariable.value.get(district)
+        if (total.isEmpty) 0 else total.get
+      }
+    }
+
+    val afterAggregation = crimes
+      .groupBy($"DISTRICT", $"STREET", $"OFFENSE_CODE")
+      .count()
+      .withColumn("ratio", $"count" / myUDF($"DISTRICT") * 100)
+
+    val afterJoining = offenseCodes
+      .join(afterAggregation, "OFFENSE_CODE")
+
+    val afterSelecting = afterJoining
+      .select($"DISTRICT", $"STREET", $"NAME", $"count".as("COUNT"), $"ratio")
+      .sort($"DISTRICT")
+    afterSelecting.createTempView("afterSelecting")
+    afterSelecting.groupBy($"DISTRICT").agg(sum($"ratio") as "ratioSum").createTempView("ratioCheck")
+    spark.sql("select * from afterSelecting").show()
+    spark.sql("select * from ratioCheck").show()
   }
 
 }
